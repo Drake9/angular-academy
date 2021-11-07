aca66183c6ece6a4ace9f155ad226a1d7cb2e69c
Paulina Kowal <paulina.kowal@globallogic.com>
2021-11-07 21:48:27 +0100

    Update books mocks

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
 import { Observable } from "rxjs";
 import { BookItem } from "../models/book-item";
-import { BooksApiService } from "../services/books-api/books-api.service";
+import { BeerService } from "../services/beer-api/beer.service";
 import { BooksService } from "../services/books/books.service";
 
 @Component({
@@ -15,28 +14,19 @@ export class BooksListComponent implements OnInit {
   books$: Observable<BookItem[]>;
   selectedId: number;
 
-  constructor(
-    private booksService: BooksService,
-    private booksApiService: BooksApiService,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private booksService: BooksService) {}
 
   ngOnInit() {
-
     this.getBooksList();
   }
 
   private getBooksList() {
-    // this.books = this.booksService.getBooksList();
+    this.books = this.booksService.getRawBooksList();
 
-    // this.booksApiService.getApiBooks().subscribe((data) => {
-    //   console.log(data);
-    // });
-
-    this.booksService
-      .getObservableOfBooksList()
-      .subscribe((books: BookItem[]) => {
-        this.books = books;
-      });
+    // this.booksService
+    //   .getObservableOfBooksList()
+    //   .subscribe((books: BookItem[]) => {
+    //     this.books = books;
+    //   });
   }
 }
--- src/app/mocks/books-mock.ts
+++ src/app/mocks/books-mock.ts
@@ -3,17 +3,33 @@ import { BookItem } from "../models/book-item";
 export const booksList: BookItem[] = [
   {
     id: 0,
-    author: "Yuval Noah Harari",
-    title: " Sapiens: A Brief History of Humankind",
+    author: "Kolk Bessel van der",
+    title: "Strach ucieleśniony",
   },
   {
     id: 2,
-    author: "Yuval Noah Harari",
-    title: " Sapiens: A Brief History of Humankind",
+    author: "Porter-Szűcs Brian ",
+    title: "Całkiem zwyczajny kraj. Historia Polski bez martyrologii",
   },
   {
     id: 3,
-    author: "Yuval Noah Harari",
-    title: " Sapiens: A Brief History of Humankind",
+    author: "Lutz Mark",
+    title: "Python. Wprowadzenie. Wydanie V",
+  },
+  {
+    id: 4,
+    author: "Bhargava Aditya",
+    title: "Algorytmy. Ilustrowany przewodnik",
+  },
+  {
+    id: 5,
+    author: "Lanier Jaron",
+    title:
+      "Dziesięć powodów, dla których powinieneś natychmiast usunąć swoje konta z mediów społecznościowych",
+  },
+  {
+    id: 6,
+    author: "McKeown Greg",
+    title: "Esencjalista",
   },
 ];
