ae683198f4c198cab1e7144a8e9874f27c34f7f2
Paulina Kowal <paulina.kowal@globallogic.com>
2021-11-08 20:18:42 +0100

    Display beers count

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { Subject } from "rxjs";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+import { filter, takeUntil } from "rxjs/operators";
+import { BeerItem } from "../models/beer-item";
+import { BeerService } from "../services/beer-api/beer.service";
 import { UserService } from "../services/user/user.service";
 
 @Component({
@@ -7,13 +10,19 @@ import { UserService } from "../services/user/user.service";
   templateUrl: "./navigation.component.html",
   styleUrls: ["./navigation.component.scss"],
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, OnDestroy {
   userName: string;
-  
-  constructor(private userService: UserService) {}
+  beersLength: number;
+  unsubscribe$: Subject<void> = new Subject<void>();
+
+  constructor(
+    private userService: UserService,
+    private beerService: BeerService
+  ) {}
 
   ngOnInit() {
     this.userNameListener();
+    this.getBeers();
   }
 
   private userNameListener() {
@@ -24,5 +33,22 @@ export class NavigationComponent implements OnInit {
 
     //approach with asyncPipe
     // this.userName$ = this.userService.userName$;
+  }
+
+  private getBeers() {
+    this.beerService
+      .getBeers()
+      .pipe(
+        filter((beers) => beers.length),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe((beers) => {
+        this.beersLength = beers.length;
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
